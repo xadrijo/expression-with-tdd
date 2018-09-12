0e00386126a93cb91c803377562c05d4038d9799
Jorge Jimenez <xadrijo@gmail.com>
2018-09-12 10:28:52 -0300

    Adding expression

--- src/Expression.php
+++ src/Expression.php
@@ -2,6 +2,8 @@
 
 class Expression
 {
+    protected $expression = '';
+
     public static function make()
     {
         return new static;
@@ -9,6 +11,57 @@ class Expression
 
     public function find($value)
     {
-        return '/' . $value . '/';
+        return $this->add($this->sanitize($value));
+    }
+
+    public function then($value)
+    {
+        return $this->find($value);
+    }
+
+    public function anything()
+    {
+        return $this->add('.*');
+    }
+
+    public function anythingBut($value)
+    {
+        $value = $this->sanitize($value);
+
+        return $this->add("(?!$value).*?");
+    }
+
+    public function maybe($value)
+    {
+        $value = $this->sanitize($value);
+
+        return $this->add("(?:$value)?");
+    }
+
+    protected function add($value)
+    {
+        $this->expression .= $value;
+
+        return $this;
+    }
+
+    public function sanitize($value)
+    {
+        return preg_quote($value, '/');
+    }
+
+    public function test($value)
+    {
+        return (bool) preg_match($this->getRegex(), $value);
+    }
+
+    public function getRegex()
+    {
+        return '/' . $this->expression . '/';
+    }
+
+    public function __toString()
+    {
+        return $this->getRegex();
     }
 }
--- tests/ExpressionTest.php
+++ tests/ExpressionTest.php
@@ -8,7 +8,46 @@ class ExpressionTest extends TestCase
     public function it_finds_a_string()
     {
         $regex = Expression::make()->find('www');
+        $this->assertTrue($regex->test('www'));
 
-        $this->assertRegExp($regex, 'www');
+        $regex = Expression::make()->then('www');
+        $this->assertTrue($regex->test('www'));
+    }
+
+    /** @test */
+    public function it_checks_for_anything()
+    {
+        $regex = Expression::make()->anything();
+
+        $this->assertTrue($regex->test('foo'));
+    }
+
+    /** @test */
+    public function it_maybe_has_a_value()
+    {
+        $regex = Expression::make()->maybe('http');
+
+        $this->assertTrue($regex->test('http'));
+
+        $this->assertTrue($regex->test(''));
+    }
+
+    /** @test */
+    public function it_can_chain_method_calls()
+    {
+        $regex = Expression::make()->find('foo')->maybe('bar')->then('biz');
+        $this->assertTrue($regex->test('foobarbiz'));
+
+        $regex = Expression::make()->find('www')->maybe('.')->then('laracast');
+        $this->assertFalse($regex->test('wwwXlaracast'));
+    }
+
+    /** @test */
+    public function it_can_exclude_values()
+    {
+        $regex = Expression::make()->find('foo')->anythingBut('bar')->then('biz');
+
+        $this->assertTrue($regex->test('foobazbiz'));
+        $this->assertFalse($regex->test('foobarbiz'));
     }
 }
